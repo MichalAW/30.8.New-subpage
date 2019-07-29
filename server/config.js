78dd295e80072188ff10d6be732feb2e234700cf
MichalAW <michal.wantaeuco@gmail.com>
2019-07-29 16:52:40 +0200

    config change

--- server/config.js
+++ server/config.js
@@ -1,7 +1,3 @@
-app.listen(config.PORT, function () {
-    console.log('Server is running on Port:', config.PORT)
-});
-
 module.exports = {
     PORT: 8000,
     DB: 'mongodb://localhost:27017/mernapp',
