d6b4eb05c0e9650c4911f20775cb3fc4d6e6a78f
MichalAW <michal.wantaeuco@gmail.com>
2019-08-07 17:51:24 +0200

    Add PostForm

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,6 +1,26 @@
 const Post = require('../models/post.model');
+const uuid = require('uuid');
 // get all posts
 // get all posts
+
+// add new post
+exports.addPost = async function (req, res) {
+
+    try {
+        const { title, author, content } = req.body;
+
+        let newPost = new Post(req.body);
+        newPost.id = uuid();
+
+        postSaved = await newPost.save();
+        res.status(200).json(postSaved);
+
+    } catch(err) {
+        res.status(500).json(err);
+    }
+
+}
+
 const getPosts = async (req, res) => {
 
     try {
